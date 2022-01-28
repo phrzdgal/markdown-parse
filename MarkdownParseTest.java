05745a86335a96e391cc87a08eaa1d628d4f5145
phrzdgal <phiroze.khattra@gmail.com>
2022-01-27 16:35:36 -0800

    8 tests were made

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -22,4 +22,79 @@ public class MarkdownParseTest {
         newList.add("some-page.html");
         assertEquals(newList, links);
     }
+
+    @Test 
+    public void test2() throws IOException {
+        Path fileName = Path.of("test2.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        ArrayList<String> newList = new ArrayList<String>();
+        newList.add("https://something.com");
+        newList.add("some-page.html");
+        assertEquals(newList, links);
+    }
+
+    @Test 
+    public void test3() throws IOException {
+        Path fileName = Path.of("test3.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        ArrayList<String> newList = new ArrayList<String>();
+        /* newList.add("https://something.com");
+        newList.add("some-page.html"); */
+        assertEquals(newList, links);
+    }
+
+    @Test 
+    public void test4() throws IOException {
+        Path fileName = Path.of("test4.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        ArrayList<String> newList = new ArrayList<String>();
+        /* newList.add("https://something.com");
+        newList.add("some-page.html"); */
+        assertEquals(newList, links);
+    }
+
+    @Test 
+    public void test5() throws IOException {
+        Path fileName = Path.of("test5.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        ArrayList<String> newList = new ArrayList<String>();
+        newList.add("page.com");
+
+        assertEquals(newList, links);
+    }
+
+    @Test 
+    public void test6() throws IOException {
+        Path fileName = Path.of("test6.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        ArrayList<String> newList = new ArrayList<String>();
+        newList.add("page.com");
+        assertEquals(newList, links);
+    }
+
+    @Test 
+    public void test7() throws IOException {
+        Path fileName = Path.of("test7.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        ArrayList<String> newList = new ArrayList<String>();
+        /* newList.add("https://something.com");
+        newList.add("some-page.html"); */
+        assertEquals(newList, links);
+    }
+
+    @Test 
+    public void test8() throws IOException {
+        Path fileName = Path.of("test8.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        ArrayList<String> newList = new ArrayList<String>();
+        newList.add("a link on the first line");
+        assertEquals(newList, links);
+    }
 }
